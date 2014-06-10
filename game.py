16995215580b70c20e0f4d593017b4db8b078b70
John <john.downs@acm.org>
2014-06-10 07:54:20 -0400

    adding game model for AI

---
+++ game.py
@@ -0,0 +1,60 @@
+def double(val):
+    if val is None:
+        return None
+    return val * 2
+
+def merge(tiles):
+    if tiles[0] == tiles[1]:
+        tiles[0], tiles[1] = double(tiles[0]), None
+    if tiles[1] == tiles[2]:
+        tiles[1], tiles[2] = double(tiles[1]), None
+    if tiles[2] == tiles[3]:
+        tiles[2], tiles[3] = double(tiles[2]), None
+    for t in range(3):
+        if tiles[t] == None:
+            tiles[t], tiles[t + 1] = tiles[t + 1], tiles[t]
+    return tiles
+
+class Game(object):
+    def __init__(self):
+        self.board = [[None, None, None, None],
+                      [None, None, None, None],
+                      [None, None, None, None],
+                      [None, None, None, None]]
+    
+    def move_left(self):
+        row_1 = merge(self.board[0])
+        row_2 = merge(self.board[1])
+        row_3 = merge(self.board[2])
+        row_4 = merge(self.board[3])
+        self.board = [row_1, row_2, row_3, row_4]
+
+    def move_right(self):
+        row_1 = merge(self.board[0][-1:])
+        row_2 = merge(self.board[1[-1:]])
+        row_3 = merge(self.board[2][-1:])
+        row_4 = merge(self.board[3][-1:])
+        self.board = [row_1[-1:], row_2[-1:], row_3[-1:], row_4[-1:]]
+
+    def move_up(self):
+        row_1 = merge([self.board[0][0], self.board[1][0], self.board[2][0], self.board[3][0]])
+        row_2 = merge([self.board[0][1], self.board[1][1], self.board[2][1], self.board[3][1]])
+        row_3 = merge([self.board[0][2], self.board[1][2], self.board[2][2], self.board[3][2]])
+        row_4 = merge([self.board[0][3], self.board[1][3], self.board[2][3], self.board[3][3]])
+        self.board = [[row_1[0], row_2[0], row_3[0], row_4[0]],
+                      [row_1[1], row_2[1], row_3[1], row_4[1]],
+                      [row_1[2], row_2[2], row_3[2], row_4[2]],
+                      [row_1[3], row_2[3], row_3[3], row_4[3]]]
+
+    def move_down(self):
+        row_1 = merge([self.board[0][0], self.board[1][0], self.board[2][0], self.board[3][0]][-1:])
+        row_2 = merge([self.board[0][1], self.board[1][1], self.board[2][1], self.board[3][1]][-1:])
+        row_3 = merge([self.board[0][2], self.board[1][2], self.board[2][2], self.board[3][2]][-1:])
+        row_4 = merge([self.board[0][3], self.board[1][3], self.board[2][3], self.board[3][3]][-1:])
+        self.board = [[row_1[0], row_2[0], row_3[0], row_4[0]][-1:],
+                      [row_1[1], row_2[1], row_3[1], row_4[1]][-1:],
+                      [row_1[2], row_2[2], row_3[2], row_4[2]][-1:],
+                      [row_1[3], row_2[3], row_3[3], row_4[3]][-1:]]
+
+        
+
---
+++ test_game.py
@@ -0,0 +1,47 @@
+from game import Game, merge
+from nose.tools import *
+
+def test_merge_dups_nones():
+    lst = [None, None, None, None]
+    result = merge(lst)
+    assert lst == result, result
+
+def test_merge_dups_no_dupes():
+    lst = [2, 4, 8, 16]
+    assert lst == merge(lst)
+
+def test_merge_dups_dupes_start():
+    lst = [2, 2, 8, 16]
+    assert [4, 8, 16, None] == merge(lst)
+
+def test_merge_dups_dupes_middle():
+    lst = [2, 8, 8, 16]
+    assert [2, 16, 16, None] == merge(lst)
+
+def test_merge_dups_dupes_end():
+    lst = [2, 8, 16, 16]
+    assert [2, 8, 32, None] == merge(lst)
+
+def test_merge_two_dups():
+    lst = [2, 2, 2, 2]
+    result = merge(lst)
+    assert [4, 4, None, None] == result, result
+
+def test_merge_two_dups_different():
+    lst = [2, 2, 4, 4]
+    assert [4, 8, None, None] == merge(lst)
+
+def test_move_left():
+    g = Game()
+    g.board = [[2, 2, 2, 2],
+               [None, 2, 2, 4],
+               [8, 16, 16, 64],
+               [None, None, 2, 4]]
+
+    expected = [[4, 4, None, None],
+               [4, 4, None, None],
+               [8, 32, 64, None],
+               [2, 4, None, None]]
+                
+    g.move_left()
+    assert_equals(g.board, expected)
